04d747d7df48c5cf40c4cd718bd7f5848207ff28
Valery Gantchev <valery@gantchev.eu>
2016-03-25 16:54:17 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'interesting_objects#index'
   devise_for :users
   resources :users
+  resources :interesting_objects do
+    member do
+      post 'rate', to: 'interesting_objects#rate'
+      post 'estimate_value', to: 'interesting_objects#estimate_value'
+    end
+  end
 end
